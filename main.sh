9ffea339ca016c61a97105bb33eb32a34f46976c
Davide Zorzella <davidezorzella100@gmail.com>
2025-06-01 10:24:25 +0200

    define program structure

---
+++ main.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Controllo dei parametri IP e porta passati tramite bash
+
+
+# Verifica che la porta sia aperta, usando `/dev/tcp`
+
+
+# Parse del risultato e output del programma aperta / chiusa
+
