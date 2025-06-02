2585be863b53a9b9c9ca9ec9393f32ebbb82b961
Davide Zorzella <davidezorzella100@gmail.com>
2025-06-02 16:06:18 +0200

    port range

--- main.sh
+++ main.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 host=${1:?"missing arg 1 for host"}
-port=${2:?"missing arg 2 for port"}
+port=${2:?"missing arg 2 for port. Accepted values: [port] | [start-end]"}
 
-echo "host address is: $host"
-echo "port number is: $port"
+if [[ $port =~ ([0-9]+)-([0-9]+) ]]; then
+  start=${BASH_REMATCH[1]}
+  end=${BASH_REMATCH[2]}
+fi
 
-timeout 1 bash -c "</dev/tcp/$host/$port" 2>/dev/null && echo "Port $port is open on $host" || echo "Port $port is closed on $host"
+for p in $(seq $start $end);
+do
+    timeout 1 bash -c "</dev/tcp/$host/$p" 2>/dev/null && echo "Port $p is open on $host" || echo "Port $p is closed on $host"
+done
